c209bbdda5732a98bbc01eea698a6ab76d8a59ae
JonasMartins <jonas.w.martins@gmail.com>
2018-08-27 21:14:56 -0300

    AA lab1 2

--- AA/lab01/example1.py
+++ AA/lab01/example1.py
@@ -1,12 +1,21 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
 from string import Template
 
 
 data = pd.read_csv('ex1data1.txt', sep=',',header=None)
 data.columns = ['col1','col2']
 
-#templ = Template("This is our data predictors:\n ${predictors}")
-#output = templ.substitute(predictors=data)
+# print(data.__doc__)
 
-print(data)
+
+
+# df = pd.DataFrame(data)
+
+plt.plot(data['col1'],data['col2'],'c.')
+plt.ylabel('column 2')
+plt.xlabel('column 1')
+plt.show()
+
