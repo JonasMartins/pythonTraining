dc1682c2d50acf87b69dbb787a7799f06ab26cd8
JonasMartins <jonas.w.martins@gmail.com>
2018-08-29 21:47:59 -0300

    AA lab1 3

--- AA/lab01/example1.py
+++ AA/lab01/example1.py
@@ -19,3 +19,14 @@ plt.ylabel('column 2')
 plt.xlabel('column 1')
 plt.show()
 
+# formula:
+# O objetivo é encontrar o w0 e w1 da reta que serviria 
+# como base para representar a distribuição dos pontos,
+# yi = w1xi + w0, onde yi é o y barra que é o valor
+# a ser achado e xi o valor de col1, temos que encontrar
+# os pesos mais adequados para essa reta melhor representar
+# o alinhamento desses pontos.
+# 
+# Após o desenvolvimento da formula 
+# j(w1,w0) = 1/(2*n) somatório de 1 até n (yi-wi * xi - w0)^2
+# iniciar os pontos w0 e w1 de forma aleatória.
