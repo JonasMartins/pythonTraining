27211d973415bfd4b17ebb2a326f364de3a99e53
JonasMartins <jonas.w.martins@gmail.com>
2018-09-01 16:48:41 -0300

    AA lab1 6

--- AA/lab01/example1.py
+++ AA/lab01/example1.py
@@ -6,10 +6,15 @@ from string import Template
 
 
 # * Método de regressão linear Univariada
-def linear_regression(df,col1:str='col1',col2:str='col2',w0:float=0.1,w1:float=0.2,alpha:float=0.01,times:int=5):
+ 
+def linear_regression_univariate(df,col1:str='col1',col2:str='col2',alpha:float=0.01,times:int=5):
     n = df.shape[0] # row number
-    W1 = w1; W0 = w0; sum_ei = 0; sum_eii = 0
     # Loop through times variable, it had default set as 5 
+    w0 = np.random.random(df.shape[1])
+    w1 = np.random.random(df.shape[1])
+    ww1 = w1; ww0 = w0; sum_ei = 0; sum_eii = 0
+
+
     for i in range(times):
         # percorrendo o dataframe
         for index, row in df.iterrows():
@@ -17,19 +22,26 @@ def linear_regression(df,col1:str='col1',col2:str='col2',w0:float=0.1,w1:float=0
             # row[col1] = xi
             yii = (w1 * row[col1]) + w0
             ei = row[col2] - yii
-            sum_ei += ei
-            sum_eii += (ei*row[col1])
+            #sum_ei += ei # normal
+            #sum_eii += (ei*row[col1]) # normal
         
-        W0 = w0 + alpha * (sum_ei/n)
-        W1 = w1 + alpha * (sum_eii/n)
+        # ww0 = w0 + alpha * (sum_ei/n) # normal
+        # ww1 = w1 + alpha * (sum_eii/n) # normal
+        
+        ww0 = w0 + alpha * ei
+        ww1 = w0
+
         # print("Run {0}: w0: {1} w1: {2}".format(i,w0,w1)) logs
-        
+        w0 = ww0
+        w1 = ww1
+        # sum_ei=sum_eii=0 # normal
+
         # updating weights    
-        w0 = w0 + (alpha * ei)
-        w1 = w1 + (alpha * (ei*row[col1]))
+        # w0 = w0 + (alpha * ei)
+        # w1 = w1 + (alpha * (ei*row[col1]))
 
         # print("W0 {0}: W1: {1}".format(w0,w1)) logs
-    return (W0,W1)
+    return (ww0,ww1)
 
 
 def main():
@@ -38,15 +50,6 @@ def main():
     data.columns = ['col1','col2']
 
     # print(data.__doc__)
-
-
-
-    # df = pd.DataFrame(data)
-
-    # plt.plot(data['col1'],data['col2'],'c.')
-    # plt.ylabel('column 2')
-    # plt.xlabel('column 1')
-    # plt.show()
 
     # formula:
     # O objetivo é encontrar o w0 e w1 da reta que serviria 
@@ -61,20 +64,25 @@ def main():
     # iniciar os pontos w0 e w1 de forma aleatória.
 
     alpha = 0.001
-    w0 = 1.1
-    w1 = 0.1
-    
-
     
     
     # atualmente sum retorna uma iteração, os primeiros w0 e w1 
-    print('Final Weights, w0,w1: {0}'.format(linear_regression(data,'col1','col2',w0,w1,alpha)))
+    # print('Final Weights, w0,w1: {0}'.format(linear_regression_univariate(data,'col1','col2',w0,w1,alpha)))
 
-    # print(n)
-    # print(data.iloc[0])
+    # y_barra = w1xi + w0 => ( (ww1*data['col1']) + ww0)
+    # times = epocas
+    times = 1000;
+    #(ww0,ww1) = linear_regression_univariate(data,'col1','col2',alpha,times)
+    
+    w0 = np.random.random(data.shape[1])
+    w1 = np.random.random(data.shape[1])
 
-    # w0 = w0 + alfa*(1/n)
+    print(w0,w1)
 
+    # plt.plot(data['col1'],data['col2'],'c.', data['col1'], ( (ww1*data['col1']) + ww0)  )
+    # plt.ylabel('column 2')
+    # plt.xlabel('column 1')
+    # plt.show()
 
 
 if __name__ == "__main__":
