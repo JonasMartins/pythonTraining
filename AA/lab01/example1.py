fc820fc77b22280619eb557f79470f26c114a60e
JonasMartins <jonas.w.martins@gmail.com>
2018-09-01 22:03:02 -0300

    AA lab1 7

--- AA/lab01/example1.py
+++ AA/lab01/example1.py
@@ -1,89 +1,90 @@
 import numpy as np
 import pandas as pd
+import random
 import matplotlib.pyplot as plt
+import sklearn
+from sklearn.datasets.samples_generator import make_regression 
+from scipy import stats
 
-from string import Template
+def gradient_descent(alpha, x, y, ep=0.0001, max_iter=1000):
+    converged = False
+    iter = 0
+    m = x.shape[0] # number of samples
+    # initial theta
+    t0 = np.random.random(x.shape[1])
+    t1 = np.random.random(x.shape[1])
 
+    # total error, J(theta)
+    # t0 + t1*x[i] funcao hipotese
+    J = sum([(t0 + t1*x[i] - y[i])**2 for i in range(m)])
 
-# * Método de regressão linear Univariada
- 
-def linear_regression_univariate(df,col1:str='col1',col2:str='col2',alpha:float=0.01,times:int=5):
-    n = df.shape[0] # row number
-    # Loop through times variable, it had default set as 5 
-    w0 = np.random.random(df.shape[1])
-    w1 = np.random.random(df.shape[1])
-    ww1 = w1; ww0 = w0; sum_ei = 0; sum_eii = 0
+    # Iterate Loop
+    while not converged:
+        # for each training sample, compute the gradient (d/d_theta j(theta))
+        grad0 = 1.0/m * sum([(t0 + t1*x[i] - y[i]) for i in range(m)]) 
+        grad1 = 1.0/m * sum([(t0 + t1*x[i] - y[i])*x[i] for i in range(m)])
 
+        # update the theta_temp
+        temp0 = t0 - alpha * grad0
+        temp1 = t1 - alpha * grad1
+    
+        # update theta
+        t0 = temp0
+        t1 = temp1
 
-    for i in range(times):
-        # percorrendo o dataframe
-        for index, row in df.iterrows():
-            # print(row[col1],row[col2])            
-            # row[col1] = xi
-            yii = (w1 * row[col1]) + w0
-            ei = row[col2] - yii
-            #sum_ei += ei # normal
-            #sum_eii += (ei*row[col1]) # normal
+        # * 38-44 para parar baseado na diferença entre J,e
+        # * método usando apenas iterações ao comentar essas linhas
+        # mean squared error
+        # e = sum( [ (t0 + t1*x[i] - y[i])**2 for i in range(m)] ) 
+
+        # if abs(J-e) <= ep:
+        #     print ('Converged, iterations: ', iter, '!!!')
+        #     converged = True
+    
+        # J = e   # update error 
         
-        # ww0 = w0 + alpha * (sum_ei/n) # normal
-        # ww1 = w1 + alpha * (sum_eii/n) # normal
-        
-        ww0 = w0 + alpha * ei
-        ww1 = w0
-
-        # print("Run {0}: w0: {1} w1: {2}".format(i,w0,w1)) logs
-        w0 = ww0
-        w1 = ww1
-        # sum_ei=sum_eii=0 # normal
-
-        # updating weights    
-        # w0 = w0 + (alpha * ei)
-        # w1 = w1 + (alpha * (ei*row[col1]))
-
-        # print("W0 {0}: W1: {1}".format(w0,w1)) logs
-    return (ww0,ww1)
-
-
-def main():
-
-    data = pd.read_csv('ex1data1.txt', sep=',',header=None)
-    data.columns = ['col1','col2']
-
-    # print(data.__doc__)
-
-    # formula:
-    # O objetivo é encontrar o w0 e w1 da reta que serviria 
-    # como base para representar a distribuição dos pontos,
-    # yi = w1xi + w0, onde yi é o y barra que é o valor
-    # a ser achado e xi o valor de col1, temos que encontrar
-    # os pesos mais adequados para essa reta melhor representar
-    # o alinhamento desses pontos.
-    # 
-    # Após o desenvolvimento da formula 
-    # j(w1,w0) = 1/(2*n) somatório de 1 até n (yi-wi * xi - w0)^2
-    # iniciar os pontos w0 e w1 de forma aleatória.
-
-    alpha = 0.001
+        iter += 1  # update iter
     
-    
-    # atualmente sum retorna uma iteração, os primeiros w0 e w1 
-    # print('Final Weights, w0,w1: {0}'.format(linear_regression_univariate(data,'col1','col2',w0,w1,alpha)))
+        if iter == max_iter:
+            print ('Max interactions exceeded!')
+            converged = True
 
-    # y_barra = w1xi + w0 => ( (ww1*data['col1']) + ww0)
-    # times = epocas
-    times = 1000;
-    #(ww0,ww1) = linear_regression_univariate(data,'col1','col2',alpha,times)
-    
-    w0 = np.random.random(data.shape[1])
-    w1 = np.random.random(data.shape[1])
+    return t0,t1
 
-    print(w0,w1)
+if __name__ == '__main__':
 
-    # plt.plot(data['col1'],data['col2'],'c.', data['col1'], ( (ww1*data['col1']) + ww0)  )
-    # plt.ylabel('column 2')
-    # plt.xlabel('column 1')
-    # plt.show()
+    # data é um array numpy.
+    data = np.loadtxt('ex1data1.txt',delimiter=',')
+    # x = da linha 0 até a ultima, pegando a coluna 0, logo os valores de x
+    x = data[:-1,[0]]
+
+    # analogamente y
+    yy = data[:-1,[1]] 
+
+    # reshape: https://docs.scipy.org/doc/numpy/reference/generated/numpy.reshape.html
+    y = yy.reshape(-1)
 
 
-if __name__ == "__main__":
-    main()
+    # x, y = make_regression(n_samples=100, n_features=1, n_informative=1, 
+    #                     random_state=0, noise=35)
+
+    alpha = 0.01 # learning rate
+    ep = 0.01 # convergence criteria
+
+    # call gredient decent, and get intercept(=theta0) and slope(=theta1)
+    theta0, theta1 = gradient_descent(alpha, x, y, ep, max_iter=1000)
+    print( ('theta0 = %s theta1 = %s') %(theta0, theta1) )
+
+    # check with scipy linear regression 
+    slope, intercept, r_value, p_value, slope_std_error = stats.linregress(x[:,0], y)
+    print( ('intercept = %s slope = %s') %(intercept, slope) )
+
+    # plot
+    for i in range(x.shape[0]):
+        y_predict = theta0 + theta1*x 
+
+    plt.plot(x,y,'.c')
+    plt.plot(x,y_predict,'k-')
+    plt.ylabel('column 2')
+    plt.xlabel('column 1')
+    plt.show()
